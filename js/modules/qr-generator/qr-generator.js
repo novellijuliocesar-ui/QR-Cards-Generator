689d5188fd5e971f04a4a01997ebc5bd0f31584f
novellijuliocesar-ui <novellijuliocesar@gmail.com>
2026-07-23 17:04:21 +0200

    Update qr-generator.js

--- js/modules/qr-generator/qr-generator.js
+++ js/modules/qr-generator/qr-generator.js
@@ -30,7 +30,7 @@ export async function initQR() {
 async function cargarExcel() {
     mostrarMensaje(elements.messageId, 'Cargando archivo Excel...', 'info', 0);
     try {
-        const response = await fetch('/mi-app-qr/data/DOC-20251215-WA0003..xlsx');
+        const response = await fetch('/QR-Cards-Generator/data/DOC-20251215-WA0003..xlsx');
         if (!response.ok) throw new Error(`HTTP ${response.status}`);
         const data = await response.arrayBuffer();
         const workbook = XLSX.read(data, { type: 'array' });
